66e9c5377b87e795a747faea64d7fba3ef3e4946
Juan <juan.carizza@gmail.com>
2017-07-15 18:48:47 -0300

    Add requirements, supervisor config file and README

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# drosh
+# Drosh
 Script to upload Shutter screenshots to Dropbox and show a notification with the shared link created.
+
+![Drosh](https://www.dropbox.com/s/2vz4xuksyoaqu4a/screencast.gif?dl=0)
+
+# Requirements
+
+- inotify
+- dropbox
+- Something to daemonize the script
+
+# Setup
+
+## Create Dropbox App
+
+Create a Dropbox App in https://www.dropbox.com/developers/apps with full access once in the OAuth2 section and create your private access token.
+
+
+## Setup with supervisor
+
+### Environment variables
+DROSH_DROPBOX_TOKEN - OAuth2 token generated in the Dropbox App
+DROSH_DROPBOX_FOLDER - Web site Dropbox folder
+DROSH_SCREENSHOT_FOLDER - Full path to your Dropbox folder where you save screenshots
+USER - Your username
+HOME - Your home path
+LOGNAME - Your username
+DISPLAY - Your X Server screen, ":0.0" by default. https://askubuntu.com/questions/432255/what-is-display-environment-variable  
+
+
+### Supervisor script
+```
+[program:drosh]
+command=<full-path-virtualenv>/bin/python <path-to-drosh>/drosh.py
+user=<your-username>
+autostart=true
+autorestart=true
+stderr_logfile=/var/log/drosh-stderr.log
+stdout_logfile=/var/log/drosh-std.log
+environment=DROSH_DROPBOX_TOKEN='<your-dropbox-token>',DROSH_DROPBOX_FOLDER='<your-dropbox-screehshots-folder>',DROSH_SCREENSHOT_FOLDER='<your-system-screenshot-folder>',USER=<your-username>,HOME=<your-home-folder>,LOGNAME=<your-username>,DISPLAY=":0.0"
+```
---
+++ src/drosh.py
@@ -0,0 +1,103 @@
+"""
+Listen to new files and create a shared link on Dropbox
+"""
+
+import sys
+import logging
+import os
+import time
+import subprocess
+
+
+import inotify.adapters
+import dropbox
+
+
+DROSH_DROPBOX_TOKEN = os.getenv('DROSH_DROPBOX_TOKEN')
+DROSH_DROPBOX_FOLDER = os.getenv('DROSH_DROPBOX_FOLDER')
+DROSH_SCREENSHOT_FOLDER = os.getenv('DROSH_SCREENSHOT_FOLDER')
+DEFAULT_LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logger = logging.getLogger(__name__)
+
+
+class Screenshot(object):
+    def __init__(self, path):
+        url = self.create_shared_link(path)
+        if url is None:
+            self.notify('Drosh', 'Error al crear link compartido')
+        else:
+            self.notify('Drosh', url)
+            
+
+    def notify(self, title, body):
+        """
+        Send a desktop message
+        """
+        subprocess.call(['notify-send', title, body])
+
+    def create_shared_link(self, path):
+        """
+        Create shared link using dropbox client
+        """
+
+        dbox = Drosh()
+        path = os.path.join(DROSH_DROPBOX_FOLDER, os.path.basename(path.decode('utf8'))) 
+        result = dbox.create_shared_link(path)
+        if result is not None:
+            logger.debug('Shared link created %r for %r file', result.url, path)
+            return result.url
+        else:
+            logger.debug('Error in create shared link for %r file', path)
+
+
+class Drosh(object):
+
+    def __init__(self, folder=DROSH_DROPBOX_FOLDER):
+        self.client = dropbox.dropbox.Dropbox(DROSH_DROPBOX_TOKEN)
+
+    def create_shared_link(self, path):
+        for i in range(6): 
+            try:
+                success = self.client.sharing_create_shared_link_with_settings(
+                    path=path,
+                    settings=None
+                )
+                return success
+                break
+            except Exception as e:
+                logger.error('Trying %s time: Error to create shared link %r', i, e)
+                time.sleep(i * 1.5)
+        
+
+def main():
+    """
+    Watch for new files where screenshots are saved and create the shared link
+    """
+
+    i = inotify.adapters.Inotify()
+    i.add_watch(bytes(DROSH_SCREENSHOT_FOLDER.encode('utf8')))
+
+    try:
+        for event in i.event_gen():
+            if event is not None:
+                (header, type_names, watch_path, filename) = event
+                if 'IN_CREATE' in type_names:
+                    logger.debug("WD=(%d) MASK=(%d) COOKIE=(%d) LEN=(%d) MASK->NAMES=%s "
+                                 "WATCH-PATH=[%s] FILENAME=[%s]",
+                                 header.wd, header.mask, header.cookie, header.len, type_names,
+                                 watch_path.decode('utf-8'), filename.decode('utf-8'))
+                    logger.info('Create shared link for: %r', filename)
+                    Screenshot(filename)
+    finally:
+        i.remove_watch(bytes(DROSH_SCREENSHOT_FOLDER.encode('utf8')))
+
+
+if __name__ == "__main__":
+
+    # Setup loggin
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    formatter = logging.Formatter(DEFAULT_LOG_FORMAT)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    main()
