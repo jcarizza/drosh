ff11f3160748453b1564e35c86b2a7c1623b95dd
Juan Carizza <juan.carizza@gmail.com>
2019-05-24 11:09:34 -0300

    Add travis flake8 and test script

--- src/tests.py
+++ src/tests.py
@@ -6,11 +6,12 @@ from unittest.mock import patch
 
 from drosh import ScreenshotHandler
 
+
 class TestScreenshotHandler(unittest.TestCase):
 
-    @patch('drosh.ScreenshotHandler.notify')
-    @patch('drosh.ScreenshotHandler.upload_file')
-    @patch('drosh.ScreenshotHandler.create_shared_link')
+    @patch("drosh.ScreenshotHandler.notify")
+    @patch("drosh.ScreenshotHandler.upload_file")
+    @patch("drosh.ScreenshotHandler.create_shared_link")
     def test_screenshot_calls(self, create_shared_link, upload_file, notify):
         """Test essential methods.
 
@@ -22,9 +23,9 @@ class TestScreenshotHandler(unittest.TestCase):
         create_shared_link.assert_called_once()
         notify.assert_called_once()
 
-    @patch('drosh.ScreenshotHandler.notify')
-    @patch('drosh.DropboxUploader.files_upload')
-    @patch('drosh.DropboxUploader.create_shared_link')
+    @patch("drosh.ScreenshotHandler.notify")
+    @patch("drosh.DropboxUploader.files_upload")
+    @patch("drosh.DropboxUploader.create_shared_link")
     def test_dropbox_uploader_methods(self, create_shared_link, files_upload, notify):
         """Test uploader.
 
@@ -32,16 +33,16 @@ class TestScreenshotHandler(unittest.TestCase):
         use the uploader to create link and upload the file"""
 
         with NamedTemporaryFile() as f:
-            url = 'https://example.com/image'
+            url = "https://example.com/image"
             name = os.path.basename(f.name)
-            upload_response = type('result', (object, ), dict(path_display=name))()
-            link_response = type('result', (object, ), dict(url=url))()
+            upload_response = type("result", (object, ), dict(path_display=name))()
+            link_response = type("result", (object, ), dict(url=url))()
             files_upload.return_value = upload_response
             create_shared_link.return_value = link_response
 
             ScreenshotHandler(f.name)
-            arg1 = os.path.join(os.getenv('DROSH_SCREENSHOT_FOLDER'), name)
-            arg2 = os.path.join(os.getenv('DROSH_DROPBOX_FOLDER'), name)
+            arg1 = os.path.join(os.getenv("DROSH_SCREENSHOT_FOLDER"), name)
+            arg2 = os.path.join(os.getenv("DROSH_DROPBOX_FOLDER"), name)
             files_upload.assert_called_once_with(arg1, arg2)
             create_shared_link.assert_called_once_with(name)
-            notify.assert_called_once_with('Drosh', url)
+            notify.assert_called_once_with("Drosh", url)
